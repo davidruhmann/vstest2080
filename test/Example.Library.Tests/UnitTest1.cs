8c713cd27373af48c83a7fd496884ac9e6185d27
David Ruhmann <druhmann@redventures.com>
2019-08-08 13:51:13 -0400

    repo project for vstest 2080

---
+++ test/Example.Library.Tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+using System;
+using Xunit;
+
+namespace Example.Library.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            Assert.True(true);
+        }
+    }
+}
